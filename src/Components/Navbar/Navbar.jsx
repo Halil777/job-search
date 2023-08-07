78a4751c42fbb37c148043bf0a02ee134248369a
Halil <microsoft7779@gmail.com>
2023-08-07 14:20:55 +0500

    first

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navBar flex justify-between items-center p-[3rem]">
+      <div className="logoDiv">
+        <h1 className="logo">
+          <strong>Job </strong>Search
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
